6475dcf78257c9251d3ad9991e8d61947fe23d87
ninoslat1 <benedict.ellnina@gmail.com>
2023-11-23 12:15:12 +0700

    update folder structure

---
+++ PasswordValidator/PasswordValidator.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PasswordValidator {
+    public partial class PasswordValidator
+    {
+        public static void Main(string[] args)
+        {      
+            Regex validateGuidRegex = MyRegex();
+            Console.Write("Enter the password: ");
+            string? input = Console.ReadLine();
+
+            if (input is null)
+                {
+                    Console.WriteLine("Error reading input.");
+                    return;
+                }
+            
+            bool result = validateGuidRegex.IsMatch(input);
+            Console.WriteLine(result);
+        }
+
+        [GeneratedRegex(
+            @"^(?=.*?[A-Z])" +          // At least one uppercase English letter
+            @"(?=.*?[a-z])" +           // At least one lowercase English letter
+            @"(?=.*?[0-9])" +           // At least one digit
+            @"(?=.*?[#?!@$%^&*-])" +    // At least one special character
+            @".{8,}$"                   // Has minimum 8 characters in length
+        )]
+        private static partial Regex MyRegex();
+    }
+}
