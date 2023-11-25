d76476be3ee7e1e54cc9e273d5a1cf5328546c70
ninoslat1 <benedict.ellnina@gmail.com>
2023-11-25 14:22:27 +0700

    add pointer palindrome

--- Palindrome/Palindrome.cs
+++ Palindrome/Palindrome.cs
@@ -5,14 +5,27 @@ public class Palindrome {
         Console.WriteLine("Type some text: ");
         string? input = Console.ReadLine();
 
-        if (input != null && input.Length != 0) {
-            CheckPalindrome(input);
-        } else { 
+        if (string.IsNullOrEmpty(input)) {
             Console.WriteLine("Please insert a text");
+            return;
+        }
+
+        var selection = AnsiConsole.Prompt(new SelectionPrompt<string>()
+                        .Title("Select palindrome mode")
+                        .PageSize(5)
+                        .MoreChoicesText("[grey](Move up and down)[/]")
+                        .AddChoices(new[] {
+                            "Pointer","Reverse"
+                        }));
+
+        if(selection == "Reverse"){
+            CheckReversePalindrome(input);
+        } else {
+            CheckPointerPalindrome(input);
         }
     }
 
-    public static void CheckPalindrome(string input) {
+    public static void CheckReversePalindrome(string input) {
         char[] stringArr = input.ToCharArray();
         Array.Reverse(stringArr);
         string reversedInput = new string (stringArr);
@@ -22,5 +35,20 @@ public class Palindrome {
         } else {
             Console.WriteLine("Not Palindrome");
         }
+    }
+
+    public static void CheckPointerPalindrome(string input) {
+        int left = 0;
+        int right = input.Length - 1;
+
+        while (left < right) {
+            if (input[left] != input[right]) {
+                Console.WriteLine("Not Palindrome");
+            }
+            left++;
+            right--;
+        }
+
+        Console.WriteLine("Palindrome");
     }
 }
